8c00d0b25f7033c9f2231509189d81e566d6b49d
karthikn <kkeyan931@gmail.com>
2022-03-28 19:22:38 +0530

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+/* eslint-disable no-undef */
+const app = Vue.createApp({
+    data() {
+      return {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john@gmail.com',
+        gender: 'male',
+        picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+      }
+    },
+    methods: {
+      async getUser() {
+        const res = await fetch('https://randomuser.me/api')
+        const { results } = await res.json()
+    
+        this.firstName = results[0].name.first
+        this.lastName = results[0].name.last
+        this.email = results[0].email
+        this.gender = results[0].gender
+        this.picture = results[0].picture.large
+      },
+    },
+  })
+  
+  app.mount('#app')
+  
